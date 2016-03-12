de322269958d554e23feee1f69428c0c42452a1e
a.kernazhytskaya <a.kernazhytskaya@softteco.com>
2016-03-12 16:45:40 +0300

    optimization part1

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ sass_dir = "styles/components"           #where our .scss files are
 images_dir = "images"    #the folder with your images
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded # After dev :compressed
+output_style = :compressed
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
